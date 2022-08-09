369284487f1bbab8f7a7310be239c19278239fa9
Charles Poullain <charles.poullain85@gmail.com>
2022-08-09 20:37:09 +0200

    Feat: Init repo with user service link

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Logger } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/user/:id')
+  getById(@Param('id') id: number) {
+    return this.appService.getUserById(id);
+  }
+
+  @Post('/user')
+  create(@Body() createItemDto) {
+    return this.appService.createUser(createItemDto);
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class AppService {
+  constructor(@Inject('USER_SERVICE') private readonly client: ClientProxy) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  createUser(createUserDto) {
+    return this.client.send({ role: 'user', cmd: 'create' }, createUserDto);
+  }
+
+  getUserById(id: number) {
+    return this.client.send({ role: 'user', cmd: 'get-by-id' }, id);
+  }
+}
